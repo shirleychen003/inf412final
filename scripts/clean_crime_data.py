ec6236250a38ebd622006e8b8478408b6c136a23
Shirley C <sshirleyy.chen@mail.utoronto.ca>
2025-03-29 11:42:48 -0400

    added TOTAL_CRIME_COUNT column

--- scripts/clean_crime_data.py
+++ scripts/clean_crime_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 crime_data = pd.read_csv("../raw_data/neighbourhood-crime-rates - 4326.csv")
 
 # Filter out the columns to keep
-columns_to_keep = ["AREA_NAME", "HOOD_ID", "ASSAULT_2020", "AUTOTHEFT_2020",
+columns_to_keep = ["AREA_NAME", "HOOD_ID", "ASSAULT_2020", "ASSAULT_RATE_2020", "AUTOTHEFT_2020",
                    "AUTOTHEFT_RATE_2020", "BIKETHEFT_2020", "BIKETHEFT_RATE_2020",
                    "BREAKENTER_2020", "BREAKENTER_RATE_2020", "HOMICIDE_2020",
                    "ASSAULT_2020", "HOMICIDE_RATE_2020", "ROBBERY_2020",
@@ -24,8 +24,13 @@ for column in cleaned_data.columns[1:]:
     else:
         cleaned_data[column] = cleaned_data[column].round(2)
 
-# Replace empty columns with 0
-cleaned_data = cleaned_data.fillna(0)
+# Add up all the crime counts
+columns_to_sum = ["ASSAULT_2020", "AUTOTHEFT_2020", "BIKETHEFT_2020", "BREAKENTER_2020",
+                  "HOMICIDE_2020", "ROBBERY_2020", "SHOOTING_2020", "THEFTFROMMV_2020",
+                  "THEFTOVER_2020"]
+
+# Create a new column 'TOTAL_CRIME_COUNT' that sums the values across the specified columns
+cleaned_data['TOTAL_CRIME_COUNT'] = cleaned_data[columns_to_sum].sum(axis=1)
 
 # Export cleaned data
 cleaned_data.to_csv("../cleaned_data/cleaned_crime_data.csv", index=False)
